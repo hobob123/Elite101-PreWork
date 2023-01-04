17daf20cf469c83baab338ca34e4b61b4487716b
Siddharth Agarwala <73672291+hobob123@users.noreply.github.com>
2023-01-04 19:43:45 +0000

    Added myMood method

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import random
 
 def response():
-  possible_responses =  ["hi",
-                         "coding is fun",
+  possible_responses =  ["Hi,",
+                         "Coding is fun.",
                          "I like cheese"]
   return random.choice(possible_responses)
 
+def myMood():
+  possible_responses =  ["happy",
+                         "angry",
+                         "sad",
+                         "excited",
+                         "bored",
+                         "irritate"]
+  return random.choiced(possible_responses)
 
 def convo():
   user_in = input("Hello! I am a chatbot. Type \"q\" to end the interaction \nHow are you?\n")
   quit_char = "q"
-  
+  print("hi")
   while (user_in != quit_char):
-    print("hi")
+    user_in = input(response() + "\n")
+    if(user_in.to_lowercase().contains("i feel")):
+      print("Oh! I feel " + myMood())
 
 
 if __name__ == "__main__":
